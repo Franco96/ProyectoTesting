ea40b3605357e73308fa5edc2831c49930985176
Lautaro Carranza <lcarranza@grupoaoniken.com.ar>
2021-11-08 13:13:06 -0300

    proyecto test terminado

--- src/Codigo/Persona.java
+++ src/Codigo/Persona.java
@@ -3,19 +3,13 @@ package Codigo;
 public class Persona {
 	
 	private String nombre;
+	private int edad;
 	
 	
-	
-	
-	
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-
-
-
-
 
 
 	public void setNombre(String nombre) {
@@ -23,13 +17,36 @@ public class Persona {
 	}
 
 
+	public int getEdad() {
+		return edad;
+	}
 
 
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 
 
 	public String saludar() {
 		
-		return "Hola soy lautaro";
+		return "Hola soy "+nombre;
+	}
+	
+	public boolean esMayorQue(Persona person) {
+		
+		if(this.edad> person.edad)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	public int cumpleaños() {
+		
+		this.edad++;
+		
+		return edad;
+		
 	}
 	
 	
--- src/tests/PersonaTest.java
+++ src/tests/PersonaTest.java
@@ -17,19 +17,44 @@ public class PersonaTest {
     public void init() {
         
        persona.setNombre("Lautaro");
+       persona.setEdad(25);
     }
 	
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+
 	
 	@Test
 	public void SaludoTest() {
 		
-		
-		assertEquals("Lautaro", persona.getNombre());
+		assertEquals("Hola soy "+persona.getNombre(), persona.saludar());
 	}
 
+	
+	@Test
+	public void EsMayorQueConPersonaMenorTest() {
+		
+		Persona personaMenor = new Persona();
+		personaMenor.setNombre("juan");
+		personaMenor.setEdad(20);
+		
+		assertEquals(true, persona.esMayorQue(personaMenor));
+	}
+	
+	@Test
+	public void EsMayorQueConPersonaMayorTest() {
+		
+		Persona personaMayor = new Persona();
+		personaMayor.setNombre("juan");
+		personaMayor.setEdad(30);
+		
+		assertEquals(false, persona.esMayorQue(personaMayor));
+	}
+	@Test
+	public void cumpleañosTest() {
+		
+		int edadCumple = persona.getEdad() +1 ;
+		
+		assertEquals(edadCumple , persona.cumpleaños());
+	}
+	
+	
 }
